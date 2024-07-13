05f0353fd0f9e4f807a21318c167745152f26384
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-07-12 23:06:22 -0400

    Add Challenge Card

---
+++ app/lesson/Card.tsx
@@ -0,0 +1,83 @@
+import { challenges } from '@/db/schema';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react';
+
+interface Props {
+  id: number;
+  imageSrc: string | null;
+  audioSrc: string | null;
+  text: string;
+  shortcut: string;
+  selected?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+  status?: 'correct' | 'wrong' | 'none';
+  type: (typeof challenges.$inferSelect)['type'];
+}
+export const Card = ({
+  id,
+  imageSrc,
+  audioSrc,
+  text,
+  shortcut,
+  selected,
+  onClick,
+  disabled,
+  status,
+  type,
+}: Props) => {
+  return (
+    <div
+      onClick={() => {}}
+      className={cn(
+        'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2',
+        selected && 'border-sky-300 bg-sky-100 hover:bg-sky-100',
+        selected &&
+          status === 'correct' &&
+          'border-green-300 bg-green-100 hover:bg-green-100',
+        selected &&
+          status === 'wrong' &&
+          'border-rose-300 bg-rose-100 hover:bg-rose-100',
+        disabled && 'pointer-events-none hover:bg-white',
+        type === 'ASSIST' && 'lg:p-3 w-full'
+      )}
+    >
+      {imageSrc && (
+        <div className='relative mb-4 aspect-square max-h-[80px] w-full lg:max-h-[150px]'>
+          <Image src={imageSrc} fill alt={text} />
+        </div>
+      )}
+      <div
+        className={cn(
+          'flex items-center justify-between',
+          type === 'ASSIST' && 'flex-row-reverse'
+        )}
+      >
+        {type === 'ASSIST' && <div />}
+        <p
+          className={cn(
+            'text-neutral-600 text-sm lg:text-base',
+            selected && 'text-sky-500',
+            selected && status === 'correct' && 'text-green-500',
+            selected && status === 'wrong' && 'text-rose-500'
+          )}
+        >
+          {text}
+        </p>
+        <div
+          className={cn(
+            'lg:w-[30px] lg:h-[30px] w-[20px] h-[20px] border-2 flex justify-center items-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold',
+            selected && 'border-sky-300 text-sky-500',
+            selected &&
+              status === 'correct' &&
+              'border-green-300 text-green-500',
+            selected && status === 'wrong' && 'border-rose-300 text-rose-500'
+          )}
+        >
+          {shortcut}
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/lesson/Quiz.tsx
+++ app/lesson/Quiz.tsx
@@ -3,6 +3,8 @@
 import { challengeOptions, challenges } from '@/db/schema';
 import { useState } from 'react';
 import { Header } from './Header';
+import { QuestionBubble } from './QuestionBubble';
+import { Challenge } from './Challenge';
 
 interface Props {
   initialPercentage: number;
@@ -23,6 +25,19 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex !== -1 ? 0 : uncompletedIndex;
+  });
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+  const title =
+    challenge.type === 'ASSIST'
+      ? 'Select the correct meaning'
+      : challenge.question;
   return (
     <>
       <Header
@@ -30,6 +45,28 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className='flex-1'>
+        <div className='flex h-full items-center justify-center'>
+          <div className='flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0'>
+            <h1 className='text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl'>
+              {title}
+            </h1>
+            <div>
+              {challenge.type === 'ASSIST' && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status='none'
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
