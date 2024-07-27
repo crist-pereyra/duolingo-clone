a9d740b33cd44076a8ce0e211b58735348413976
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-07-27 10:50:53 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: '/lingo-preview.png',
   },
+  themeColor: '#22c55e', // Added themeColor property matching bg-green-500
 };
 
 export default function RootLayout({
